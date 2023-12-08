566384c975038666b6fe74400c273c27bebe9a7e
sebelumcahahaya <anandadimmas1204@gmail.com>
2023-12-09 00:49:01 +0700

    first commit

---
+++ Footer.blade.php
@@ -0,0 +1,44 @@
+<div class="footer">
+    <div class="py-6 px-6">
+        <p class="mb-0 fs-4">
+            Design and Developed by
+            <a href="https://adminmart.com/" target="_blank"
+                class="pe-1 text-primary text-decoration-underline">AdminMart.com</a>
+            Distributed by <a href="https://themewagon.com">ThemeWagon</a>
+        </p>
+    </div>
+</div>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .page-wrapper {
+        flex: 1;
+        padding-bottom: 60px;
+        /* Adjust this value based on your footer height */
+    }
+
+    .footer {
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        /* background-color: #f8f9fa; */
+        /* Set your desired background color */
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        text-align: center;
+    }
+
+    .footer p {
+        margin: 0;
+        /* Remove default margin for the paragraph */
+    }
+</style>
---
+++ Script.blade.php
@@ -0,0 +1,8 @@
+   <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+   <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+   <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+   <script src="{{ asset('assets/js/app.min.js') }}"></script>
+   <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+   <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+   <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
